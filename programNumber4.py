53451d5b76769c648d37ccc7fb4df88325dffbb3
unknown <janroanabad5@gmail.com>
2024-05-03 10:21:31 +0800

    My third commit

--- programNumber4.py
+++ programNumber4.py
@@ -12,3 +12,10 @@ def process_integers(input_file):
     # Process integers and create lists of squares and cubes
     even_squares = [int(num) ** 2 for num in integers if int(num) % 2 == 0]
     odd_cubes = [int(num) ** 3 for num in integers if int(num) % 2 != 0]
+
+    # Write results to output files
+    with open('double.txt', 'w') as f:
+        f.write('\n'.join(map(str, even_squares)))
+
+    with open('triple.txt', 'w') as f:
+        f.write('\n'.join(map(str, odd_cubes)))
