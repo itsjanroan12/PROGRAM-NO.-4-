da172cb86e8910266620369431bce80115f49ba0
unknown <janroanabad5@gmail.com>
2024-05-03 11:17:29 +0800

    My first commit

---
+++ programNumber1.py
@@ -0,0 +1,6 @@
+#Creator Information: 
+#Name: Abad, Jan Roan D.
+#Section: BSCpE 1-2
+#Date: April 30, 2024
+#Faculty: Prof. Danilo Madigalejos
+#Program Description: "a Python program that reads a text file named numbers.txt that contains 20 integers. The program will create two other text file; the first text file will be named even.txt that will contains all even numbers extracted from the numbers.txt. The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt."
