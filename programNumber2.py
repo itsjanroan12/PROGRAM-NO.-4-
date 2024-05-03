be98e674c12afbc63deb6223f3e3bc0a2e3bf19f
unknown <janroanabad5@gmail.com>
2024-05-03 09:49:57 +0800

    My first commit

---
+++ programNumber2.py
@@ -0,0 +1,6 @@
+#Creator Information: 
+#Name: Abad, Jan Roan D.
+#Section: BSCpE 1-2
+#Date: April 30, 2024
+#Faculty: Prof. Danilo Madigalejos
+#Program Description: " A Python program that read a file containing the name of 20 students together with their GWA. The program will outputs the name of the student who got the highest GWA (including the GWA)."
