2dc423f550c932ee4993edd2c9173335d60b8895
unknown <janroanabad5@gmail.com>
2024-05-03 11:37:40 +0800

    My second commit

--- programNumber1.py
+++ programNumber1.py
@@ -4,3 +4,10 @@
 #Date: April 30, 2024
 #Faculty: Prof. Danilo Madigalejos
 #Program Description: "a Python program that reads a text file named numbers.txt that contains 20 integers. The program will create two other text file; the first text file will be named even.txt that will contains all even numbers extracted from the numbers.txt. The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt.git git "
+def main():
+    # Open the file for reading
+    with open("numbers.txt", "r") as file:
+        numbers = file.readlines()
+
+    # Convert strings to integers
+    numbers = [int(num.strip()) for num in numbers]
