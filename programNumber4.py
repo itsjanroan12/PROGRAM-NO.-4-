be6056d2b4cc0847a293f2e1a19349d2e03f596c
unknown <janroanabad5@gmail.com>
2024-05-03 10:19:17 +0800

    My second commit

--- programNumber4.py
+++ programNumber4.py
@@ -4,3 +4,7 @@
 #Date: April 30, 2024
 #Faculty: Prof. Danilo Madigalejos
 #Program Description: "A method in python that will create two separate text files after reading the source text file named integers.txt that contains 20 integers. The first output file will be named double.txt containing the square of all even integers found in integers.txt and the second file will be named triple.txt containing the cube of all odd numbers found in the integers.txt."
+def process_integers(input_file):
+    # Open the source file for reading
+    with open(input_file, 'r') as f:
+        integers = f.readlines()
