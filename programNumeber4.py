f2472ba99559f1b2e14266c27f02392617f0110f
unknown <janroanabad5@gmail.com>
2024-05-03 10:33:40 +0800

    My first commit

---
+++ programNumeber4.py
@@ -0,0 +1,6 @@
+#Creator Information: 
+#Name: Abad, Jan Roan D.
+#Section: BSCpE 1-2
+#Date: April 30, 2024
+#Faculty: Prof. Danilo Madigalejos
+#Program Description: "A method in python that will create two separate text files after reading the source text file named integers.txt that contains 20 integers. The first output file will be named double.txt containing the square of all even integers found in integers.txt and the second file will be named triple.txt containing the cube of all odd numbers found in the integers.txt."
