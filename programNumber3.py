2f66615daf215a523493d2138dbd474c98003f98
unknown <janroanabad5@gmail.com>
2024-05-11 09:43:57 +0800

    My first commit

---
+++ programNumber3.py
@@ -0,0 +1,6 @@
+#Creator Information: 
+#Name: Abad, Jan Roan D.
+#Section: BSCpE 1-2
+#Date: April 30, 2024
+#Faculty: Prof. Danilo Madigalejos
+#Program Description: "a method in python to write multiple line of text contents into a text file mylife.txt. See sample output:
